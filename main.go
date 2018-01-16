d393b272a4455a4ac2fd6ba2e20166a678ad5dbd
sinmetal <metal.tie@gmail.com>
2018-01-16 18:14:22 +0900

    HTTP Errorはpanicで死なないように

--- main.go
+++ main.go
@@ -3,13 +3,19 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
 	const t = "http://10.128.0.2"
-	resp, err := http.Get(t)
-	if err != nil {
-		panic(err)
+
+	for {
+		time.Sleep(time.Second * 3)
+		resp, err := http.Get(t)
+		if err != nil {
+			fmt.Printf("GET request to %s and the result was failed\n", t)
+			continue
+		}
+		fmt.Printf("GET request to %s and the result was %s\n", t, resp.Status)
 	}
-	fmt.Printf("GET request to %s and the result was %s\n", t, resp.Status)
 }
