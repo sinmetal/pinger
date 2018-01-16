ce60892346cb370e323f35c826ffc7d3f8a85f7d
sinmetal <metal.tie@gmail.com>
2018-01-16 14:39:58 +0900

    DockerからICMPは出れなかったからHTTPになった・・・

--- main.go
+++ main.go
@@ -1,27 +1,14 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-
-	"github.com/sparrc/go-ping"
+	"net/http"
 )
 
 func main() {
-	const t = "www.google.com"
-	fmt.Println(t)
-
-	pinger, err := ping.NewPinger(t)
+	resp, err := http.Get("http://10.142.0.2")
 	if err != nil {
 		panic(err)
 	}
-	pinger.Count = 3
-	pinger.Run()                 // blocks until finished
-	stats := pinger.Statistics() // get send/receive/rtt stats
-
-	b, err := json.Marshal(stats)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(string(b))
+	fmt.Println(resp.Status)
 }
